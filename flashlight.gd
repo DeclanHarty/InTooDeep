f682853f35dcb4ae5763865e45cf6077c2291286
unknown <declanharty04@gmail.com>
2024-10-26 12:41:42 -0400

    Improved the flashlight

--- flashlight.gd
+++ flashlight.gd
@@ -1,6 +1,10 @@
 extends PointLight2D
 
 var current_direction : int = 1
+const LIGHT_DISTANCE_FROM_PLAYER : float = 244.0
+const LIGHT_ROTATION_SPEED : float = 1
+var target_light_position : Vector2 = Vector2(0,0)
+var rotation_target : float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +14,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotation = atan(position.y/position.x)
 
 
-func _on_character_body_2d_direction_changed(new_direction: Variant) -> void:
-	if(sign(new_direction.x) != current_direction):
-		position.x = -position.x
-		texture
-		current_direction = sign(new_direction.x)
+func _on_character_body_2d_direction_changed(velocity: Vector2) -> void:
+	if(sign(velocity.x) != 0 and sign(velocity.x) != current_direction):
+		#position.x = -position.x
+		scale.x = -scale.x
+		current_direction = sign(velocity.x)
+		
+	if(velocity):
+		position = velocity.normalized() * LIGHT_DISTANCE_FROM_PLAYER
