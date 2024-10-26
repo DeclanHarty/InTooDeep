002030fa98799adfc0052100f1dc1dd7d5ab011c
Ryan Shu <89924964+rrs6624@users.noreply.github.com>
2024-10-26 14:56:45 -0400

    changed oxygen manager
    signal isn't being sent

--- scripts/oxygen_manager.gd
+++ scripts/oxygen_manager.gd
@@ -8,6 +8,12 @@ signal oxygen_changed(oxygen: int)
 var _oxygen_tank: float
 var _is_depleting = false
 
+var is_in_water : bool = true
+
+func _on_water_detection_water_state_changed(in_water: bool) -> void:
+	print("signal received")
+	self.is_in_water = in_water
+
 # separate public methods to deplete and stop depleting
 func start_depleting():
 	_is_depleting = true
@@ -22,6 +28,17 @@ func _ready():
 	_oxygen_tank = oxygen_max
 
 func _process(delta: float):
+	
+	if (is_in_water == true):
+		_is_depleting = true
+		
+	elif (is_in_water != false):
+		_is_depleting = false
+
 	if (_is_depleting):
 		_oxygen_tank -= oxygen_depletion_per_seconds * delta
+		oxygen_changed.emit(_oxygen_tank)
+		
+	elif (!_is_depleting):
+		_oxygen_tank += oxygen_depletion_per_seconds * delta
 		oxygen_changed.emit(_oxygen_tank)
