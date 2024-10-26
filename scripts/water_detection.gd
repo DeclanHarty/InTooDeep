d6a27d27d9f4aceb9d31f8de16db36caa8310bbd
25Pi25 <25pi25@mail.com>
2024-10-26 18:13:15 -0400

    reorganized tile mapping

--- scripts/water_detection.gd
+++ scripts/water_detection.gd
@@ -3,11 +3,11 @@ extends Area2D
 var in_water : bool = true
 # Called when the node enters the scene tree for the first time.
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	print("Entered air bubble")
 	%OxygenManager.stop_depleting()
 
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_area_2d_body_exited(_body: Node2D) -> void:
 	%OxygenManager.start_depleting()
 	print("Exited air bubble")
