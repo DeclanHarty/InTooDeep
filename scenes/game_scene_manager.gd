42aa80b13cd3a3dbbb534044fd4da7148c1bce44
unknown <declanharty04@gmail.com>
2024-10-27 13:39:49 -0400

    Working on Audio

--- scenes/game_scene_manager.gd
+++ scenes/game_scene_manager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var game_over_screen = preload("res://scenes/game_over_screen.tscn")
 
 func _on_oxygen_manager_out_of_oxygen() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_packed(game_over_screen)
