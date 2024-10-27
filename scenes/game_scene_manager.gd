edafed465e0b608c9badc39e142aa2893863d498
unknown <declanharty04@gmail.com>
2024-10-27 14:40:31 -0400

    Added win collider

--- scenes/game_scene_manager.gd
+++ scenes/game_scene_manager.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 var game_over_screen = preload("res://scenes/game_over_screen.tscn")
+var win_screen = preload("res://scenes/game_over_screen.tscn")
 
 func _on_oxygen_manager_out_of_oxygen() -> void:
 	get_tree().change_scene_to_packed(game_over_screen)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_packed(win_screen)
